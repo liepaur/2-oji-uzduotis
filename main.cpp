5792342b6f8f4cb5e11d15ef6bdf1b5e9dec2186
liepaur <liepa.urbonaite@mif.stud.vu.lt>
2025-10-09 22:03:04 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,18 @@
+#include "funkcijos.h"
 #include <iostream>
 #include <vector>
 #include <cstdlib>
 #include <ctime>
-#include "funkcijos.h"
+#include <chrono>
+#include "Timer.h"
+
 using namespace std;
 
 int main(){
     srand(time(0));
-    vector<Studentas> studentai;
+    vector<Studentas> studentai, kietiakai, vargsiukai;
+    Timer t;
+    Timer visas;
     int pasirinkimas;
 
     cout << "Pasirinkite duomenų šaltinį:" << endl
@@ -16,8 +21,31 @@ int main(){
     cin >> pasirinkimas;
 
     if (pasirinkimas == 1){
-        nuskaitymas("studentai1000000.txt", studentai);
-    } 
+        cout << "Pasirinkite failo dydį" << endl;
+        cout << "1 - 1000 įrašų" << endl;
+        cout << "2 - 10000 įrašų" << endl;
+        cout << "3 - 100000 įrašų" << endl;
+        cout << "4 - 1000000 įrašų" << endl;
+        cout << "5 - 10000000 įrašų" << endl;
+        int failoPasirinkimas;
+        cin >> failoPasirinkimas;
+        auto start = std::chrono::high_resolution_clock::now();
+        if (failoPasirinkimas == 1)
+            nuskaitymas("studentai1000.txt", studentai);
+        else if (failoPasirinkimas == 2)
+            nuskaitymas("studentai10000.txt", studentai);
+        else if (failoPasirinkimas == 3)
+            nuskaitymas("studentai100000.txt", studentai);
+        else if (failoPasirinkimas == 4)
+            nuskaitymas("studentai1000000.txt", studentai);
+        else if (failoPasirinkimas == 5)    
+            nuskaitymas("studentai10000000.txt", studentai);
+        else{
+            cout << "Tokio pasirinkimo nėra!" << endl;
+        }
+        cout << studentai.size() << " Duomenų nuskaitymas užtruko: " << t.elapsed() << " s\n";
+    }
+
     else if (pasirinkimas == 2){
         int kiekis, ndKiekis;
         cout << "Įveskite studentų skaičių: ";
@@ -31,6 +59,22 @@ int main(){
         return 0;
     }
 
+    t.reset();
+    grupavimas(studentai, kietiakai, vargsiukai);
+    cout << studentai.size() << " Duomenų rūšiavimas užtruko: " << t.elapsed() << " s\n";
+    
+    t.reset();
+    sugrupuotuSpausdinimas("kietiakai.txt", kietiakai);
+    cout << "Kietiakų spausdinimas užtruko: " << t.elapsed() << " s\n";
+    
+    t.reset();
+    cout << "Vargsiųjų spausdinimas užtruko: ";
+    sugrupuotuSpausdinimas("vargsiukai.txt", vargsiukai);
+
+    cout << "Duomenų įrašymas į failus užtruko: " << t.elapsed() << " s\n";
+    
     spausdinimas(studentai);
     return 0;
+
+    cout << studentai.size() << " Visas programos vykdymas užtruko: " << visas.elapsed() << " s\n";
 }
