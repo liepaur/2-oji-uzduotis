c72fda8ab51ab5549801451e465eb91b17822bae
liepaur <liepa.urbonaite@mif.stud.vu.lt>
2025-11-13 17:24:45 +0200

    pakoregavau main.cpp faila

--- main.cpp
+++ main.cpp
@@ -1,121 +1,108 @@
 #include "funkcijos.h"
+#include "Studentas.h"
+#include "Timer.h"
 #include <iostream>
 #include <vector>
 #include <cstdlib>
 #include <ctime>
 #include <chrono>
-#include "Timer.h"
-#include "studentas.h"
 
-using namespace std;
-
-int main(){
+int main() {
     srand(time(0));
-    list<Studentas> studentai, kietiakai, vargsiukai, galutinis;
-    Timer t;
     Timer visas;
+    std::vector<Studentas> studentai, kietiakai, vargsiukai;
+
     int pasirinkimas = 0;
-    
-    cout << "Pasirinkite duomenų šaltinį:" << endl
-        << "1 - Skaityti iš failo" << endl
-        << "2 - Generuoti atsitiktinai" << endl;
-        cin >> pasirinkimas;
+    std::cout << "Pasirinkite duomenų šaltinį:\n"
+              << "1 - Skaityti iš failo\n"
+              << "2 - Generuoti atsitiktinai\n";
+    std::cin >> pasirinkimas;
 
-    if (pasirinkimas == 1){
+    if (pasirinkimas == 1) {
         int failoPasirinkimas;
-        cout << "Pasirinkite failo dydį" << endl;
-        cout << "1 - 1000 įrašų" << endl;
-        cout << "2 - 10000 įrašų" << endl;
-        cout << "3 - 100000 įrašų" << endl;
-        cout << "4 - 1000000 įrašų" << endl;
-        cout << "5 - 10000000 įrašų" << endl;
-        cout << flush;
-        cin >> failoPasirinkimas;
+        std::cout << "Pasirinkite failo dydį:\n"
+                  << "1 - 1000 įrašų\n"
+                  << "2 - 10000 įrašų\n"
+                  << "3 - 100000 įrašų\n"
+                  << "4 - 1000000 įrašų\n"
+                  << "5 - 10000000 įrašų\n";
+        std::cin >> failoPasirinkimas;
 
-        auto start = std::chrono::high_resolution_clock::now();
+        std::string failoVardas;
         if (failoPasirinkimas == 1)
-            nuskaitymas("studentai1000.txt", studentai);
+            failoVardas = "studentai1000.txt";
         else if (failoPasirinkimas == 2)
-            nuskaitymas("studentai10000.txt", studentai);
+            failoVardas = "studentai10000.txt";
         else if (failoPasirinkimas == 3)
-            nuskaitymas("studentai100000.txt", studentai);
+            failoVardas = "studentai100000.txt";
         else if (failoPasirinkimas == 4)
-            nuskaitymas("studentai1000000.txt", studentai);
-        else if (failoPasirinkimas == 5)    
-            nuskaitymas("studentai10000000.txt", studentai);
-        else{
-            cout << "Tokio pasirinkimo nėra!" << endl;
+            failoVardas = "studentai1000000.txt";
+        else if (failoPasirinkimas == 5)
+            failoVardas = "studentai10000000.txt";
+        else {
+            std::cout << "Tokio pasirinkimo nėra!\n";
             return 0;
         }
-        cout << studentai.size() << " Duomenų nuskaitymas užtruko: " << t.elapsed() << " s\n";
 
+        Timer t;
+        nuskaitymas(failoVardas, studentai);
+        std::cout << studentai.size() << " studentų nuskaityta per " << t.elapsed() << " s.\n";
     }
-
-    else if (pasirinkimas == 2){
+    else if (pasirinkimas == 2) {
         int kiekis, ndKiekis;
-        cout << "Įveskite studentų skaičių: ";
-        cin >> kiekis;
-        cout << "Įveskite namų darbų skaičių: ";
-        cin >> ndKiekis;
+        std::cout << "Įveskite studentų skaičių: ";
+        std::cin >> kiekis;
+        std::cout << "Įveskite namų darbų skaičių: ";
+        std::cin >> ndKiekis;
         studentuGeneravimas(studentai, kiekis, ndKiekis);
     }
-
-    else{
-        cout << "Tokio pasirinkimo nėra!" << endl;
+    else {
+        std::cout << "Tokio pasirinkimo nėra!\n";
         return 0;
     }
 
-    cout << "Pasirinkite galutinio balo skaičiavimo būdą:" << endl
-        << "1 - Vidurkis" << endl
-        << "2 - Mediana" << endl;
     int GalutinioPasirinkimas;
-    cin >> GalutinioPasirinkimas;
-    if(GalutinioPasirinkimas !=1 && GalutinioPasirinkimas !=2){
-        cout << "Tokio pasirinkimo nėra!" << endl;
-        return 0;
-    }
-    else if(GalutinioPasirinkimas == 1){
-        for(auto& s : studentai){
-            s.galutinis = baloSkaiciavimas(s);
-        }
-    }
-    else if(GalutinioPasirinkimas == 2){
-        for(auto& s : studentai){
-            s.galutinis = baloSkaiciavimasMediana(s);
-        }
-    }
+    std::cout << "Pasirinkite galutinio balo skaičiavimo būdą:\n"
+              << "1 - Vidurkis\n"
+              << "2 - Mediana\n";
+    std::cin >> GalutinioPasirinkimas;
 
-    cout << "Pasirinkite duomenų rūšiavimo būdą:" << endl
-        << "1 - Pagal vardą" << endl
-        << "2 - Pagal pavardę" << endl
-        << "3 - Pagal galutinį balą" << endl;
+    for (auto& s : studentai)
+        s.apskaiciuotiGalutini(GalutinioPasirinkimas == 2); // true = mediana, false = vidurkis
+
     int RusiavimoPasirinkimas;
-    cin >> RusiavimoPasirinkimas;
-    if(RusiavimoPasirinkimas !=1 && RusiavimoPasirinkimas !=2 && RusiavimoPasirinkimas !=3){
-        cout << "Tokio pasirinkimo nėra!" << endl;
+    std::cout << "Pasirinkite duomenų rūšiavimo būdą:\n"
+              << "1 - Pagal vardą\n"
+              << "2 - Pagal pavardę\n"
+              << "3 - Pagal galutinį balą\n";
+    std::cin >> RusiavimoPasirinkimas;
+
+    if (RusiavimoPasirinkimas == 1)
+        std::sort(studentai.begin(), studentai.end(), palyginimasVardas);
+    else if (RusiavimoPasirinkimas == 2)
+        std::sort(studentai.begin(), studentai.end(), palyginimasPavarde);
+    else if (RusiavimoPasirinkimas == 3)
+        std::sort(studentai.begin(), studentai.end(), palyginimasGalutinis);
+    else {
+        std::cout << "Tokio pasirinkimo nėra!\n";
         return 0;
     }
-    else if(RusiavimoPasirinkimas == 1)
-        studentai.sort(palyginimasVardas);
-    else if(RusiavimoPasirinkimas == 2)
-        studentai.sort(palyginimasPavarde);
-    else if(RusiavimoPasirinkimas == 3)
-        studentai.sort(palyginimasGalutinis);
 
-    t.reset();
+    Timer t;
     grupavimas(studentai, kietiakai, vargsiukai);
-    cout << studentai.size() << " Duomenų rūšiavimas užtruko: " << t.elapsed() << " s\n";
-    
-    t.reset();
-    sugrupuotuSpausdinimas("kietiahkai.txt", kietiakai);
-    cout << "Kietiakų spausdinimas užtruko: " << t.elapsed() << " s\n";
-    
-    t.reset();
-    cout << "Vargsiukų spausdinimas užtruko: " << t.elapsed() << " s\n";
-    sugrupuotuSpausdinimas("vargsiukai.txt", vargsiukai);
+    std::cout << studentai.size() << " studentų rūšiavimas užtruko: " << t.elapsed() << " s.\n";
 
-    cout << "Duomenų įrašymas į failus užtruko: " << t.elapsed() << " s\n";
-    
+    t.reset();
+    sugrupuotuSpausdinimas("kietiakai.txt", kietiakai);
+    std::cout << "Kietiakų spausdinimas užtruko: " << t.elapsed() << " s.\n";
+
+    t.reset();
+    sugrupuotuSpausdinimas("vargsiukai.txt", vargsiukai);
+    std::cout << "Vargsiukų spausdinimas užtruko: " << t.elapsed() << " s.\n";
+
+    t.reset();
     spausdinimas(studentai);
-    cout << studentai.size() << " Duomenų visos programos vykdymas užtruko: " << visas.elapsed() << " s\n";
+    std::cout << studentai.size() << " studentų rezultatų išvedimas užtruko: " << t.elapsed() << " s.\n";
+
+    std::cout << "Visa programa užtruko: " << visas.elapsed() << " s.\n";
 }
