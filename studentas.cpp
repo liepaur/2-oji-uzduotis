d1c227db6bba174e31c820be9e3b7a17c3a6e973
liepaur <liepa.urbonaite@mif.stud.vu.lt>
2025-11-13 16:22:33 +0200

    pakeičiau funkcijas studentas.cpp faile

--- studentas.cpp
+++ studentas.cpp
@@ -2,43 +2,64 @@
 #include <algorithm>
 #include <iterator>
 
-double baloSkaiciavimas(const Studentas& s) {
-    double suma = 0;
-    for (int paz : s.nd) suma += paz;
-    double nd_vidurkis = static_cast<double>(suma) / s.nd.size();
-    return 0.4 * nd_vidurkis + 0.6 * s.egz;
-}
-
-double baloSkaiciavimasMediana(const Studentas& s) {
-    if (s.nd.empty()) return 0.0;
-    list<int> kopija = s.nd;
-    kopija.sort();
-    size_t n = kopija.size();
-    auto it = kopija.begin();
-    
-    double mediana;
+double mediana(std::vector<int> v) {
+    if (v.empty()) return 0.0;
+    std::sort(v.begin(), v.end());
+    size_t n = v.size();
     if (n % 2 == 0) {
-        advance(it, n / 2 - 1);
-        double first = *it;
-        advance(it, 1);
-        double second = *it;
-        mediana = (first + second) / 2.0;
+        return (v[n / 2 - 1] + v[n / 2]) / 2.0;
     }
     else {
-        advance(it, n / 2);
-        mediana = *it;
+        return v[n / 2];
     }
-    return 0.4 * mediana + 0.6 * s.egz;
+}
+
+std::istream& Studentas::nuskaitymas(std::istream& is) {
+    is >> vardas_ >> pavarde_;
+    int paz;
+    nd_.clear();
+    while (is >> paz) {
+        nd_.push_back(paz);
+    }
+
+    if(!nd_.empty()) {
+        egz_ = nd_.back();
+        nd_.pop_back();
+    } else {
+        egz_ = 0;
+    }
+    return is;
+}
+
+double Studentas::skaiciuotiVidurki() const {
+    if (nd_.empty()) return 0.0;
+    double vidurkis = std::accumulate(nd_.begin(), nd_.end(), 0.0) / nd_.size();
+    return vidurkis;
+}
+
+double Studentas::skaiciuotiMediana() const {
+    return mediana(nd_);
+}
+
+void Studentas::apskaiciuotiGalutini(bool naudotiMediana) {
+    double nd_rezultatas = naudotiMediana ? skaiciuotiMediana() : skaiciuotiVidurki();
+    galutinis_ = 0.4 * nd_rezultatas + 0.6 * egz_;
+}
+
+void Studentas::spausdinimas(std::ostream& os) const {
+    os << std::left << std::setw(15) << vardas_
+       << std::left << std::setw(15) << pavarde_
+       << std::right << std::setw(5) << std::fixed << std::setprecision(2) << galutinis_;
 }
 
 bool palyginimasVardas(const Studentas &a, const Studentas &b) {
-    return a.vardas < b.vardas;
+    return a.vardas() < b.vardas();
 }
 
 bool palyginimasPavarde(const Studentas &a, const Studentas &b) {
-    return a.pavarde < b.pavarde;
+    return a.pavarde() < b.pavarde();
 }
 
 bool palyginimasGalutinis(const Studentas &a, const Studentas &b) {
-    return a.galutinis < b.galutinis;
+    return a.galutinis() < b.galutinis();
 }
