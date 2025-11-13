38e906c256d2c2e8ec2c6e71952f5b5b47ca2760
liepaur <liepa.urbonaite@mif.stud.vu.lt>
2025-11-13 16:26:48 +0200

    sutvarkiau funkcijos.h funkcijas

--- funkcijos.h
+++ funkcijos.h
@@ -7,11 +7,10 @@
 
 using namespace std;
 
-
-void nuskaitymas(const string& failoVardas, list<Studentas>& studentai);
-void studentuGeneravimas(list<Studentas>& studentai, int kiekis, int ndKiekis);
-void grupavimas(list<Studentas>& studentai, list<Studentas>& kietiakai, list<Studentas>& vargsiukai);
-void sugrupuotuSpausdinimas(const string& failoVardas, list<Studentas>& studentai);
-void spausdinimas(list<Studentas>& studentai);
+void nuskaitymas(const std::string& failoVardas, std::vector<Studentas>& studentai);
+void studentuGeneravimas(std::vector<Studentas>& studentai, int kiekis, int ndKiekis);
+void grupavimas(std::vector<Studentas>& studentai, std::vector<Studentas>& kietiakai, std::vector<Studentas>& vargsiukai);
+void sugrupuotuSpausdinimas(const string& failoVardas, std::vector<Studentas>& studentai);
+void spausdinimas(std::vector<Studentas>& studentai);
 
 #endif
